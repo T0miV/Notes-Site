89226a4dded24bcbe74b0a45105b7bfd37893a28
Tomi <tomi_99@hotmail.fi>
2025-01-11 16:03:02 +0200

    yes

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -9,20 +9,20 @@ dotenv.config();
 
 // Luo Express-sovellus
 const app = express();
-const port = process.env.PORT || 5000; // Portti voidaan asettaa ympäristömuuttujasta
+const port = process.env.PORT || 5000;
 
 // Middlewaret
 app.use(cors());
 app.use(express.json());
 
+// Pääsivureitti
+app.get('/', (req, res) => {
+  res.send('Backend is running'); // Tähän voit laittaa viestin, joka kertoo että palvelin on käynnissä
+});
+
 // Reitit
 app.use('/users', userRoutes);
 app.use('/notes', noteRoutes);
-
-// Pääsivu ("/") näyttää "Backend is running" -viestin
-app.get('/', (req, res) => {
-  res.send('<h1>Backend is running</h1>');  // HTML-elementti näyttää tekstin
-});
 
 if (process.env.NODE_ENV !== 'production') {
   // Paikallinen kehitysympäristö: käynnistä palvelin
