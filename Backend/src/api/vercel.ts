a171d873c739d25b5b00245dc019c2358a73cdf3
Tomi <tomi_99@hotmail.fi>
2025-01-11 13:35:39 +0200

    yes

--- Backend/src/api/vercel.ts
+++ Backend/src/api/vercel.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../src/server'; // Importoi Express-sovellus server.ts:stä
+import app from '../server'; // Importoi Express-sovellus server.ts:stä
 
 // Wrapper Vercelin serverless-funktiolle
 export default (req: VercelRequest, res: VercelResponse) => {
