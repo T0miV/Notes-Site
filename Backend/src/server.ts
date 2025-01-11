8c7515d7f5eaaa227fb9651842d094eab9249541
Tomi <tomi_99@hotmail.fi>
2025-01-11 17:31:01 +0200

    yes

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -18,5 +18,13 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/notes', noteRoutes);
 
+// Aja serveri vain paikallisessa ympäristössä
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
 // Exportoi Express-sovellus
 export default app;
